766625693716fd42e50f5f873f16a87c261a92f2
mrsemyon <syoma4k@gmail.com>
2022-02-19 05:56:13 +0300

    Moved code from index files to header

--- create_user/index.php
+++ create_user/index.php
@@ -7,39 +7,12 @@ if ($_SESSION['role'] != 'admin') {
     redirect('/users/');
     exit;
 }
+
+$title = "Добавить пользователя";
+
+include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <meta name="description" content="Chartist.html">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
-    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="/assets/css/vendors.bundle.css">
-    <link id="appbundle" rel="stylesheet" media="screen, print" href="/assets/css/app.bundle.css">
-    <link id="myskin" rel="stylesheet" media="screen, print" href="/assets/css/skins/skin-master.css">
-    <link rel="stylesheet" media="screen, print" href="/assets/css/fa-solid.css">
-    <link rel="stylesheet" media="screen, print" href="/assets/css/fa-brands.css">
-    <link rel="stylesheet" media="screen, print" href="/assets/css/fa-regular.css">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="/assets/img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/users/">Главная <span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">               
-                <li class="nav-item">
-                    <a class="nav-link" href="/login/logout.php">Выйти</a>
-                </li>
-             </ul>
-        </div>
-    </nav>
     <main id="js-page-content" role="main" class="page-content mt-3">
     <?php if(isset($_SESSION['danger'])):?>
                 <div class="alert alert-danger text-dark" role="alert">
