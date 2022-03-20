f3d33fbfb154b3be27cef2f050cf5a792d6be4b7
mrSemyon <sky_net@mail.com>
2022-03-20 11:35:28 +0500

    Bugfix in editing credentials with admin role

--- security/security.php
+++ security/security.php
@@ -11,21 +11,23 @@ if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
     exit;
 }
 
-if (empty($_POST['password']) && ($_SESSION['email'] == $_POST['email'])) {
+if (empty($_POST['password']) && ($user['email'] == $_POST['email'])) {
     setFlashMessage('danger', 'Информация не была обновлена');
     redirect('/users/');
     exit;
 }
 
 if (! empty($_POST['email'])) {
-    if ($_POST['email'] != $_SESSION['email']) {
+    if ($_POST['email'] != $user['email']) {
         if (!empty(getUserByEmail($pdo, $_POST['email']))) {
             setFlashMessage('danger', 'Этот адрес уже занят другим пользователем.');
             redirect("/users/");
             exit;
         }
         changeEmail($pdo, $user['id'], $_POST['email']);
-        $_SESSION['email'] = $_POST['email'];
+        if ($_SESSION['role'] != 'admin') {
+            $_SESSION['email'] = $_POST['email'];
+        }
     }
 }
 
