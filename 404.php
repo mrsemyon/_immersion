944191f55df5e4466f8b892f66df1342070e9a35
mrSemyon <syoma4k@gmail.com>
2022-04-10 19:01:17 +0500

    Created page 404 & .htaccess

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$title = "Страница не найдена";
+
+include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+?>
+
+        <main id="js-page-content" role="main" class="page-content mt-3">
+            <div class="row">
+                <div class="col-xl-12">
+                    <h1>Сраница не найдена</h1>
+                </div>
+            </div>
+        </div>
+        </main>
+    </body>
+</html>
