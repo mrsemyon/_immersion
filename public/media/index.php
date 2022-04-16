82652476ddf747994692c4ec76908330c2d3c5a0
mrSemyon <syoma4k@gmail.com>
2022-04-16 20:00:15 +0500

    Moved indexes files to own folders

---
+++ public/media/index.php
@@ -0,0 +1,84 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$user = $db->read('users', $_GET);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'У Вас недостаточно прав');
+    redirect("/public/users/");
+    exit;
+}
+
+$title = "Редактировать аватар";
+
+include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+?>
+    <main id="js-page-content" role="main" class="page-content mt-3">
+        <div class="subheader">
+            <h1 class="subheader-title">
+                <i class='subheader-icon fal fa-image'></i> Загрузить аватар
+            </h1>
+
+        </div>
+        <form action="/controllers/media.php?id=<?=$user['id']?>" method="POST" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-xl-6">
+                    <div id="panel-1" class="panel">
+                        <div class="panel-container">
+                            <div class="panel-hdr">
+                                <h2>Текущий аватар</h2>
+                            </div>
+                            <div class="panel-content">
+                                <div class="form-group">
+                                    <img src="/upload/<?=$user['photo']?>" alt="" class="img-responsive" width="200">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="example-fileinput">Выберите аватар</label>
+                                    <input name="photo" type="file" id="example-fileinput" class="form-control-file">
+                                </div>
+
+                                <div class="col-md-12 mt-3 d-flex flex-row-reverse">
+                                    <button class="btn btn-warning">Загрузить</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
+
+    <script src="js/vendors.bundle.js"></script>
+    <script src="js/app.bundle.js"></script>
+    <script>
+
+        $(document).ready(function()
+        {
+
+            $('input[type=radio][name=contactview]').change(function()
+                {
+                    if (this.value == 'grid')
+                    {
+                        $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-g');
+                        $('#js-contacts .col-xl-12').removeClassPrefix('col-xl-').addClass('col-xl-4');
+                        $('#js-contacts .js-expand-btn').addClass('d-none');
+                        $('#js-contacts .card-body + .card-body').addClass('show');
+
+                    }
+                    else if (this.value == 'table')
+                    {
+                        $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-1');
+                        $('#js-contacts .col-xl-4').removeClassPrefix('col-xl-').addClass('col-xl-12');
+                        $('#js-contacts .js-expand-btn').removeClass('d-none');
+                        $('#js-contacts .card-body + .card-body').removeClass('show');
+                    }
+
+                });
+
+                //initialize filter
+                initApp.listFilter($('#js-contacts'), $('#js-filter-contacts'));
+        });
+
+    </script>
+</body>
+</html>
---
+++ public/status/index.php
@@ -0,0 +1,97 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/src/core.php';
+
+$user = $db->read('users', $_GET);
+
+if (($_SESSION['role'] != 'admin') && ($_SESSION['email'] != $user['email'])) {
+    setFlashMessage('danger', 'У Вас недостаточно прав');
+    redirect("/public/users/");
+    exit;
+}
+
+$statuses = [
+    "success" => "Онлайн",
+    "warning" => "Отошёл",
+    "danger" => "Не беспокоить",
+    "unknown" => "Не установлен",
+];
+
+$title = "Редактировать статус";
+
+include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+?>
+    <main id="js-page-content" role="main" class="page-content mt-3">
+        <div class="subheader">
+            <h1 class="subheader-title">
+                <i class='subheader-icon fal fa-sun'></i> Установить статус
+            </h1>
+
+        </div>
+        <form action="/controllers/status.php?id=<?=$user['id']?>" method="POST">
+            <div class="row">
+                <div class="col-xl-6">
+                    <div id="panel-1" class="panel">
+                        <div class="panel-container">
+                            <div class="panel-hdr">
+                                <h2>Установка текущего статуса</h2>
+                            </div>
+                            <div class="panel-content">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <!-- status -->
+                                        <div class="form-group">
+                                            <label class="form-label" for="example-select">Выберите статус</label>
+                                            <select name="status" class="form-control" id="example-select">
+                                                <?php foreach ($statuses as $key => $value) { ?>
+                                                    <option <?=($user['status'] == $key) ? 'selected' : ''?> value="<?=$key?>"><?=$value?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 mt-3 d-flex flex-row-reverse">
+                                        <button class="btn btn-warning">Set Status</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
+
+    <script src="js/vendors.bundle.js"></script>
+    <script src="js/app.bundle.js"></script>
+    <script>
+
+        $(document).ready(function()
+        {
+
+            $('input[type=radio][name=contactview]').change(function()
+                {
+                    if (this.value == 'grid')
+                    {
+                        $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-g');
+                        $('#js-contacts .col-xl-12').removeClassPrefix('col-xl-').addClass('col-xl-4');
+                        $('#js-contacts .js-expand-btn').addClass('d-none');
+                        $('#js-contacts .card-body + .card-body').addClass('show');
+
+                    }
+                    else if (this.value == 'table')
+                    {
+                        $('#js-contacts .card').removeClassPrefix('mb-').addClass('mb-1');
+                        $('#js-contacts .col-xl-4').removeClassPrefix('col-xl-').addClass('col-xl-12');
+                        $('#js-contacts .js-expand-btn').removeClass('d-none');
+                        $('#js-contacts .card-body + .card-body').removeClass('show');
+                    }
+
+                });
+
+                //initialize filter
+                initApp.listFilter($('#js-contacts'), $('#js-filter-contacts'));
+        });
+
+    </script>
+</body>
+</html>
